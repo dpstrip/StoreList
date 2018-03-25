b5eeb2613fef0fe742b91b2895033df638942ddb
David Stripeik <dpstrip@outlook.com>
2018-03-25 13:22:40 -0500

    Defined item class

---
+++ StoreList/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreList.Models
+{
+    public class Item
+    {
+        public LiteDB.ObjectId _id { get; set; }
+        string name { get; set; } //Name of item
+        string location { get; set; } //location of item
+        Category cat { get; set; } //category
+        int quantity { get; set; } //Quantity of the item
+    }
+}
